2596805f46427ba99243e3aba228362496fb5df5
Matthew Campbell <matthewtyronecampbell@gmail.com>
2025-09-24 16:20:17 +1200

    made all crypto currency's get requested in one api request It only prints out the json file that gets recieved from the api.

--- main.py
+++ main.py
@@ -4,16 +4,26 @@ api_key_file = open("/home/matthew/Code/API Keys/CoinRanking.txt")
 API_KEY = api_key_file.readline().strip()
 api_key_file.close()
 
-URL = 'https://api.coinranking.com/v2/coin/{uuid}/price'
 UUID_CODES = {'Doge':'a91GCGd_u96cF', 'Bitcoin':'Qwsogvtv82FCd', 'Ethereum':'razxDUgYGNAdQ'}
-
 HEADER = {
     'Content-Type': 'application/json',
     'x-access-token': f'{API_KEY}'
 }
-def get_crypto_price(url,header):
-    response = requests.get(url,headers=header).json()
-    return response['data']['price']
+def get_crypto_data(header,uuid_codes):
+    # The array element variable is the text required to add another uuid code into the url
+    # example: https://api.coinranking.com/v2/coins?uuids[]=razxDUgYGNAdQ&uuids[]=Qwsogvtv82FCd
+    url_array_element = "{and_symbol}uuids[]={uuid}"
+    url = 'https://api.coinranking.com/v2/coins?'
+    # this adds the different cryptocurriences to the api url. Because of the & needed for additional ones, the first crypto is done outside of the loop
+    
+    for x in uuid_codes:
+        if x == 'Doge':
+            url = url+url_array_element.format(and_symbol="", uuid=uuid_codes[x])
+            continue
+        url = url+url_array_element.format(and_symbol="&", uuid=uuid_codes[x])
+    print(url)
+    response = requests.get(url,headers=header)
+    return response
 
 def menu():
     user_response = input("Press Enter to get the latest Crypto prices: ")
@@ -22,8 +32,6 @@ def menu():
         print("Program exited")
     else:
         print('Extracting crypto prices ...')
-        for x in UUID_CODES:
-            crypto_prices[x] = get_crypto_price(URL.format(uuid=UUID_CODES[x]),HEADER)
-        for x in crypto_prices:
-            print(f'{x}: ${crypto_prices[x]}')
+        crypto_data = get_crypto_data(HEADER,UUID_CODES)
+        print(crypto_data.text)
 menu()
