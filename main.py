20a3dfc583f7eabf94123e56af4822b69e5422b2
Matthew Campbell <matthewtyronecampbell@gmail.com>
2025-09-25 15:35:57 +1200

    extracted prices from api response

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ api_key_file = open("/home/matthew/Code/API Keys/CoinRanking.txt")
 API_KEY = api_key_file.readline().strip()
 api_key_file.close()
 
-UUID_CODES = ['a91GCGd_u96cF', 'Qwsogvtv82FCd', 'Qwsogvtv82FCd',]
+UUID_CODES = ['a91GCGd_u96cF', 'Qwsogvtv82FCd', 'razxDUgYGNAdQ',]
 HEADER = {
     'Content-Type': 'application/json',
     'x-access-token': f'{API_KEY}'
@@ -22,13 +22,21 @@ def get_crypto_data(header,uuid_codes):
     response = requests.get(url,headers=header)
     return response
 
+def get_crypto_prices(response):
+    response_json = response.json()
+    crypto_coins = response_json["data"]["coins"]
+    prices = {}
+    for crypto in crypto_coins:
+        prices[crypto["name"]] = crypto["price"]
+    return prices
+
 def menu():
     user_response = input("Press Enter to get the latest Crypto prices: ")
-    crypto_prices = {}
     if user_response == True:
         print("Program exited")
     else:
         print('Extracting crypto prices ...')
-        crypto_data = get_crypto_data(HEADER,UUID_CODES)
-        print(crypto_data.text)
+        prices = get_crypto_prices(get_crypto_data(HEADER, UUID_CODES))
+        for x in prices:
+            print(f"{x}: ${prices[x]}")
 menu()
