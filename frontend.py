234752a835b50018f217323bb58233a6fd7aeee1
Matthew Campbell <matthewtyronecampbell@gmail.com>
2025-10-02 16:46:25 +1300

    made a basic UI

--- frontend.py
+++ frontend.py
@@ -1,19 +1,18 @@
 from backend import Crypto, get_api_key, menu
 
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 
-class DisplayCrypto(GridLayout):
-     def __init__(self, **kwargs):
-        super(DisplayCrypto, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text="bitcoin"))
-        self.add_widget(Label(text="Doge"))
+crypto_object = Crypto(get_api_key())
+crypto_prices = crypto_object.get_crypto_prices()
 class MyApp(App):
 
     def build(self):
-        return DisplayCrypto()
+        layout = BoxLayout(orientation='vertical')
+        for x in crypto_prices:
+            layout.add_widget(Button(text=f"{x}: ${crypto_prices[x]}", background_color=(18, 94, 145, 0.37)))
+        return layout
 
 
 if __name__ == '__main__':
