284b3d3f08c06de620a277240fc9bd442457c73e
Matthew Campbell <matthewtyronecampbell@gmail.com>
2025-10-04 12:03:04 +1300

    made class variable for putting the request data, so it can be accesseb by other functions. get_crypto_data needs to be called first. all other functions fetch data from self.crypto_data

--- backend.py
+++ backend.py
@@ -15,6 +15,7 @@ class Crypto:
                     'Content-Type': 'application/json',
                     'x-access-token': f'{self.api_key}'
                     }
+        self.crypto_data = {}
     def get_crypto_data(self):
         # The array element variable is the text required to add another uuid code into the url
         # example: https://api.coinranking.com/v2/coins?uuids[]=razxDUgYGNAdQ&uuids[]=Qwsogvtv82FCd
@@ -26,21 +27,20 @@ class Crypto:
         for code in self.UUID_CODES:
             url = url+url_array_element.format(code)
             url_array_element = "&uuids[]={}"
+        # makes api request and puts it in self.crypto_data
         response = requests.get(url,headers=self.HEADER)
-        return response
+        self.crypto_data = response.json()
     
     # gets the crypto prices and puts them in a dictionary, with the name of the coin as the key
     def get_crypto_prices(self):
-        response_json = self.get_crypto_data().json()
-        if response_json["status"] == "error":
-            raise RuntimeError(response_json["message"])
-        crypto_coins = response_json["data"]["coins"]
+        if self.crypto_data["status"] == "error":
+            raise RuntimeError(self.crypto_data["message"])
+        crypto_coins = self.crypto_data["data"]["coins"]
         prices = {}
         for crypto in crypto_coins:
             prices[crypto["name"]] = round(float(crypto["price"]),3)
         return prices
     
-
 #basic cammand line interface 
 def menu(crypto_object):
     # if user presses enter user_response will equal false, and program will continue
@@ -50,9 +50,11 @@ def menu(crypto_object):
     else:
         print('Extracting crypto prices ...')
         try:
+            crypto_object.get_crypto_data()
             prices = crypto_object.get_crypto_prices()
             for x in prices:
                 print(f"{x}: ${prices[x]}")
         except RuntimeError as error:
             print(error)   
-
+if __name__ == '__main__':
+    menu(Crypto(get_api_key()))
